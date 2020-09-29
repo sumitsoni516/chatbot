4d878fde899325a80320b93fa1e875bf0fc225c8
Sumit <sumitsn590@gmail.com>
2020-09-30 01:38:21 +0530

    fetch url changed

--- css/index.js
+++ css/index.js
@@ -67,7 +67,7 @@ function serverMessage(response2) {
 
 function fetchmsg(){
 
-     var url = 'http://localhost:5055/send-msg';
+     var url = '/send-msg';
       
       const data = new URLSearchParams();
       for (const pair of new FormData(document.getElementById("mymsg"))) {
